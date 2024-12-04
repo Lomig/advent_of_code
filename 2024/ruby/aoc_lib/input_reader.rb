686c1b54b0f5dfc29b8cea796aa17cf3e5c9bf1a
Lomig Enfroy <lomig.enfroy@jobteaser.com>
2024-12-04 20:19:19 +0100

    Solve Day 04 in Ruby

--- 2024/ruby/aoc_lib/input_reader.rb
+++ 2024/ruby/aoc_lib/input_reader.rb
@@ -3,9 +3,10 @@
 require "matrix"
 
 class RawInputReader
-  private attr_reader :file_name, :formatter
+  private attr_reader :input, :file_name, :formatter
 
-  def initialize(file_name:, formatter:)
+  def initialize(input:, file_name:, formatter:)
+    @input = input
     @file_name = file_name
     @formatter = formatter
   end
@@ -18,23 +19,25 @@ class RawInputReader
     end
   end
 
-  def raw_input = File.read(file_name)
+  def raw_input = input || File.read(file_name)
 end
 
 class ArrayReader < RawInputReader
-  def read = File.readlines(file_name, chomp: true).map(&formatter)
+  def read = lines.map(&formatter)
+
+  def lines = input&.split("\n") || File.readlines(file_name, chomp: true)
 end
 
 class MatrixReader < RawInputReader
   private attr_reader :row, :column
 
-  def initialize(file_name:, formatter:)
+  def initialize(input:, file_name:, formatter:)
     @row = @column = 0
     super
   end
 
   def read
-    File.open(file_name)
+    (input || File.open(file_name))
       .each_char
       .with_object([[]], &populate_matrix)
       .then { |matrix| Matrix[*matrix] }
@@ -79,7 +82,7 @@ class InputReader
 
   private attr_reader :input
 
-  def self.new(file_name:, structure:, formatter:)
-    INPUT_STRUCTURES[structure].new(file_name:, formatter:)
+  def self.new(input:, file_name:, structure:, formatter:)
+    INPUT_STRUCTURES[structure].new(input:, file_name:, formatter:)
   end
 end
